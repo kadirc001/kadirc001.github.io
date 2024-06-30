a220e0c760a699464a1d18525ffe3e7864127ab5
Kadir <kadirc@tuta.io>
2024-06-30 22:26:23 +0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className="title">Kadir Celik</h1>
+        <p>Welcome to my portfolio!</p>
+      </div>
+      <div>
+        <h1>About</h1>
+        <p>
+          I am a <strong>frontend</strong> and <strong>android</strong>{" "}
+          developer, an open source enthusiast and a{" "}
+          <strong>Linux (I use arch btw)</strong> user from Turkey
+        </p>
+      </div>
+      <div>
+        <h1>Tech stack</h1>
+        <ul>
+          <li>
+            <strong>Languages</strong>: Rust, Python, Javascript, Html, Css and
+            Shell
+          </li>
+          <li>
+            <strong>Frameworks</strong>: Tokio, Rocket, Node.js, Bun.js, React,
+            React Native, Expo and Next.js
+          </li>
+          <li>
+            <strong>Tools</strong>: Git, Bruno, Android Studio and Neovim
+          </li>
+        </ul>
+        <p>
+          Visit my <a href="https://github.com/kadirc001/dotfiles">dotfiles</a>{" "}
+          to learn more
+        </p>
+      </div>
+      <div>
+        <h1>Connect</h1>
+        <ul>
+          <li>
+            <strong>Mail</strong>:{" "}
+            <a href="mailt:kadirc@tuta.io">kadirc@tuta.io</a>
+          </li>
+          <li>
+            <strong>Github</strong>:{" "}
+            <a href="https://github.com/kadirc001">kadirc001</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
